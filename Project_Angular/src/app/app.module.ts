30a371b344e5ed54fb62bbc5b0f0e35ddd698182
Kalichhe <jhondh2003@gmail.com>
2023-04-17 12:21:07 -0500

    Algunas cosas

--- Project_Angular/src/app/app.module.ts
+++ Project_Angular/src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { Routes } from '@angular/router';
+
+const appRoutes:Routes=[
+
+  {path:"login", component:LoginComponent}
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
