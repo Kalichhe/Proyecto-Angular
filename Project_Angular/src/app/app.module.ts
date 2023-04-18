faea603e945f44400951587ef4af2e9b2f714a6e
Kalichhe <jhondh2003@gmail.com>
2023-04-17 22:14:06 -0500

    Nuevos componentes y barra superior en los demas

--- Project_Angular/src/app/app.module.ts
+++ Project_Angular/src/app/app.module.ts
@@ -2,28 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { Routes } from '@angular/router';
-import { RegisterComponent } from './register/register.component';
+import { RouterModule, Routes } from '@angular/router';
 import { LogInComponent } from './log-in/log-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
+import { FormsModule } from '@angular/forms';
+import { AdministratorComponent } from './administrator/administrator.component';
 
 const appRoutes:Routes=[
 
-  {path:"login", component:LoginComponent}
+  {path:"", component:LogInComponent},
+  {path:"sign-up", component:SignUpComponent},
+  {path:"terms-and-conditions", component:TermsAndConditionsComponent},
+  {path:"administrator", component:AdministratorComponent},
 
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     LogInComponent,
-    SignUpComponent
+    SignUpComponent,
+    TermsAndConditionsComponent,
+    AdministratorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes) // Sirve para enrutar las paginas
   ],
   providers: [],
   bootstrap: [AppComponent]
