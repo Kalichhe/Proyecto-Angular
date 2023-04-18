dc0f9201ef870f9f9523f67876493121df5a8f9c
Kalichhe <jhondh2003@gmail.com>
2023-04-17 19:18:13 -0500

    Camcios de nombres de los componentes

--- Project_Angular/src/app/app.module.ts
+++ Project_Angular/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const appRoutes:Routes=[
 
@@ -14,7 +17,10 @@ const appRoutes:Routes=[
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    LogInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule
